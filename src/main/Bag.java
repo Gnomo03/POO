8c38e8ae955bfa94779d307fbe306f1ec0c5c7e6
SenseiBott <jotapastore@gmail.com>
2023-04-18 18:16:11 +0100

    Updated classes

---
+++ src/main/Bag.java
@@ -0,0 +1,179 @@
+/**
+ * Represents a Bag item that extends the Item class.
+ * It has instance variables such as dimension, material and releaseDate.
+ * The class has constructors, getters, and setters for its instance
+ * variables.
+ */
+public class Bag extends Item {
+    private double dimension;
+    private String material;
+    private int releaseDate;
+
+    /**
+     * Default constructor for the Bag class.
+     */
+    public Bag() {
+        super();
+        this.dimension = 0;
+        this.material = "n/d";
+        this.releaseDate = 0;
+    }
+
+    /**
+     * Constructor for the Bag class with all parameters.
+     *
+     * @param description     The description of the bag.
+     * @param brand           The brand of the bag.
+     * @param reference       The reference of the bag.
+     * @param basePrice       The base price of the bag.
+     * @param priceCorrection The price correction of the bag.
+     * @param carrier         The carrier of the bag.
+     * @param conditionScore  The condition score of the bag.
+     * @param previousOwners  The number of previous owners of the bag.
+     * @param premiumStat     Whether or not the bag is premium status.
+     * @param dimension       The dimension of the bag.
+     * @param material        The material of the bag.
+     * @param releaseDate     The release date of the bag.
+     */
+    public Bag(String description, String brand, String reference, double basePrice, double priceCorrection,
+            Carrier carrier, double conditionScore, int previousOwners, boolean premiumStat, double dimension,
+            String material, int releaseDate,int userId) {
+        super(description, brand, reference, basePrice, priceCorrection, carrier, conditionScore, previousOwners,
+                premiumStat,userId);
+        this.dimension = dimension;
+        this.material = material;
+        this.releaseDate = releaseDate;
+    }
+
+    /**
+     * Copy constructor for the Bag class.
+     * 
+     * @param oneBag The Bag object to be copied.
+     */
+    public Bag(Bag oneBag) {
+        super(oneBag);
+        this.dimension = oneBag.getDimension();
+        this.material = oneBag.getMaterial();
+        this.releaseDate = oneBag.getReleaseDate();
+    }
+
+    /**
+     * Returns the dimension of the bag.
+     * 
+     * @return The dimension of the bag.
+     */
+    public double getDimension() {
+        return this.dimension;
+    }
+
+    /**
+     * Returns the material of the bag.
+     * 
+     * @return The material of the bag.
+     */
+    public String getMaterial() {
+        return this.material;
+    }
+
+    /**
+     * Returns the release date of the bag.
+     * 
+     * @return The release date of the bag.
+     */
+    public int getReleaseDate() {
+        return this.releaseDate;
+    }
+
+    /**
+     * Returns the price of the bag based on its value (premium or not) and on the
+     * release date.
+     * 
+     * @return The price of the bag.
+     */
+    public double getPrice() {
+        if (this.isPremium())
+            return (10 + (2023 - this.releaseDate)) / 10 * this.getBasePrice();
+        else {
+            return (this.getBasePrice() / this.dimension);
+        }
+    }
+
+    /**
+     * Sets the dimension of the bag.
+     * 
+     * @param dimension The dimension to set for the bag.
+     */
+    public void setDimension(double dimension) {
+        this.dimension = dimension;
+    }
+
+    /**
+     * Sets the material of the bag.
+     * 
+     * @param material The material to set for the bag.
+     */
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    /**
+     * Sets the release date of the bag.
+     * 
+     * @param releaseDate The release date to set for the bag.
+     */
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    /**
+     * Returns a string representation of the Bag.
+     *
+     * @return a string representation of the Bag
+     */
+    public String toString() {
+        return "Bag{" +
+                "description='" + getDescription() + '\'' +
+                ", brand='" + getBrand() + '\'' +
+                ", reference='" + getReference() + '\'' +
+                ", basePrice=" + getBasePrice() +
+                ", priceCorrection=" + getPriceCorrection() +
+                ", carrier='" + getCarrier() + '\'' +
+                ", conditionScore=" + getConditionScore() +
+                ", previousOwners=" + getPreviousOwners() +
+                ", premiumStat=" + isPremium() +
+                ", dimension=" + this.dimension +
+                ", material=" + this.material +
+                ", releaseDate=" + this.releaseDate +
+                '}';
+    }
+
+    /**
+     * This method checks if the object o is equal to the current object.
+     * 
+     * @param o the object to compare with the current object
+     * @return true if the objects are equal and false otherwise
+     */
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (o == null || o.getClass() != this.getClass())
+            return false;
+        Bag s = (Bag) o;
+        return this.getDescription().equals(s.getDescription()) && this.getBrand().equals(s.getBrand())
+                && this.getReference().equals(s.getReference()) && this.getBasePrice() == s.getBasePrice()
+                && this.getPriceCorrection() == s.getPriceCorrection() && this.getCarrier().equals(s.getCarrier())
+                && this.getConditionScore() == s.getConditionScore()
+                && this.getPreviousOwners() == s.getPreviousOwners() && this.isPremium() == (s.isPremium())
+                && this.dimension == s.getDimension() && this.material.equals(s.getMaterial())
+                && this.releaseDate == s.getReleaseDate();
+    }
+
+    /**
+     * This method creates and returns a copy of the current object.
+     * 
+     * @return a copy of the current object
+     */
+    public Bag clone() {
+        return new Bag(this);
+    }
+}
---
+++ src/main/Order.java
@@ -0,0 +1,194 @@
+import java.time.LocalDate;
+import java.util.List;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Order {
+    private List<Item> collection;
+    private HashMap<String,Integer> carrierHelper;
+    private TypeOfSize dimension;
+    private double itemPrice;
+    private double satisfactionPrice;
+    private OrderState state;
+    private LocalDate date;
+    private int id;
+
+    private static int currentID = 0;
+
+    public enum TypeOfSize{
+        Little, Medium, Big;
+    }
+
+    public enum OrderState {
+        Pending, Finished, Dispatched;
+    }
+    
+    public Order(){
+        this.collection = new LinkedList<Item>();
+        this.carrierHelper = new HashMap<String,Integer>();
+        this.dimension = TypeOfSize.Little;
+        this.itemPrice = 0;
+        this.satisfactionPrice = 0;
+        this.state = OrderState.Pending;
+        this.date = LocalDate.now();
+        this.id = currentID;
+        currentID++;
+    }
+
+    public Order(List<Item> collection,HashMap<String,Integer> carrierHelper,TypeOfSize dimension,double satisfactionPrice,double itemPrice, OrderState state, LocalDate date){
+        this.collection = collection;
+        this.dimension = dimension;
+        this.carrierHelper = carrierHelper;
+        this.itemPrice = itemPrice;
+        this.satisfactionPrice = satisfactionPrice;
+        this.state = state;
+        this.date = date;
+        this.id = currentID;
+        currentID++;
+    }
+
+    public Order(Order oneOrder){
+        this.collection = oneOrder.getCollection();
+        this.dimension = oneOrder.getDimension();
+        this.itemPrice = oneOrder.getItemPrice();
+        this.carrierHelper = oneOrder.getCarrierHelper();
+        this.satisfactionPrice = oneOrder.getSatisfactionPrice();
+        this.state = oneOrder.getState();
+        this.date = oneOrder.getDate();
+        this.id = oneOrder.getID();
+    }
+    public double getSatisfactionPrice(){
+        return this.satisfactionPrice;
+    }
+    public HashMap<String,Integer> getCarrierHelper(){
+        return this.carrierHelper;
+    }
+
+    public List<Item> getCollection(){
+        return this.collection;
+    }
+
+    public TypeOfSize getDimension(){
+        return this.dimension;
+    }
+
+    public double getItemPrice(){
+        return this.itemPrice;
+    }
+
+    public OrderState getState(){
+        return this.state;
+    }
+
+    public LocalDate getDate(){
+        return this.date;
+    }
+
+    public int getID(){
+        return this.id;
+    }
+
+    public void setCollection(List<Item> collection){
+        this.collection = collection;
+    }
+
+    public void setDimension(TypeOfSize dimension){
+        this.dimension = dimension;
+    }
+
+    public void setItemPrice(double itemPrice){
+        this.itemPrice = itemPrice;
+    }
+    public void setSatisfactionPrice(double Price){
+        this.satisfactionPrice = Price;
+    }
+
+    public void setState(OrderState state){
+        this.state = state;
+    }
+
+    public void setDate(LocalDate date){
+        this.date = date;
+    }
+
+    public String toString(){
+        return "Order{" +
+               "collection='" +this.collection.toString()+ "\'" +
+               "dimension='" +this.dimension+
+               "Final Price='" +this.itemPrice+
+               "Satisfaction Price='" +this.satisfactionPrice+
+               "State='" +this.state+
+               "Date='" +this.date.toString()+ "\'" +
+               "ID='" +this.id+ "}";
+    }
+    
+    public boolean equals(Object o){
+        if (o == this)
+            return true;
+        if(o == null || o.getClass() != this.getClass())
+            return false;
+        Order e = (Order) o;
+        return this.getCollection().equals(e.getCollection()) &&
+               this.getDimension().equals(e.getDimension()) &&
+               this.getItemPrice() == e.getItemPrice() &&
+               this.getSatisfactionPrice() == e.getSatisfactionPrice() &&
+               this.getState().equals(e.getState()) &&
+               this.getID() == e.getID();
+    }
+
+    public Order clone(){
+        return new Order(this);
+    }
+
+    public void addItem(Item oneItem){
+        int nmbr = this.collection.size();
+            if(nmbr ==1)
+                this.dimension = TypeOfSize.Medium;
+            if(nmbr ==5)
+                this.dimension = TypeOfSize.Big;
+        this.collection.add(oneItem);
+        this.itemPrice += oneItem.getPrice(); // Adicionar valor do pedido
+
+        if(this.carrierHelper.containsKey(oneItem.getCarrier().getName()))
+        this.carrierHelper.put(oneItem.getCarrier().getName(), this.carrierHelper.get(oneItem.getCarrier().getName()) + 1);
+        else 
+        this.carrierHelper.put(oneItem.getCarrier().getName(), 1);
+    
+        if(oneItem.getConditionScore()==1) 
+            this.satisfactionPrice+=0.5;
+        else {
+            this.satisfactionPrice+=0.25;
+        }
+    }
+
+    public void removeItem(Item oneItem){
+        int nmbr = this.collection.size();
+            if(nmbr ==2)
+                this.dimension = TypeOfSize.Little;
+            if(nmbr ==6)
+                this.dimension = TypeOfSize.Medium;
+        this.collection.remove(oneItem);
+        this.itemPrice -= oneItem.getPrice();
+
+        this.carrierHelper.put(oneItem.getCarrier().getName(), this.carrierHelper.get(oneItem.getCarrier().getName()) - 1);
+    }
+
+    public double calculateFinalPrice(){
+        double tax = 0;
+        for(Item i : this.collection){
+
+            int many = this.carrierHelper.get(i.getCarrier().getName());
+
+                if (many == 1)
+                    tax += i.getPrice()*i.getCarrier().getTaxSmall();
+                if (many >= 2 && many <= 5)
+                    tax += i.getPrice()*i.getCarrier().getTaxMedium();
+                if (many >5)
+                    tax += i.getPrice()*i.getCarrier().getTaxBig();
+
+        }
+        
+        return this.itemPrice+this.satisfactionPrice+tax;
+    }
+
+}
