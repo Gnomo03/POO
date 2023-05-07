0103e49573bb007b2c889beb30b53b9091d4d7df
SenseiBott <jotapastore@gmail.com>
2023-05-07 17:26:29 +0100

    Premium fix

--- src/main/app/PremiumSneaker.java
+++ src/main/app/PremiumSneaker.java
@@ -1,40 +1,40 @@
 package app;
+
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Stack;
 
+public class PremiumSneaker extends Sneaker implements Premium {
 
-public class PremiumSneaker extends Sneaker {
-
-    public PremiumSneaker(){
+  public PremiumSneaker() {
     super();
-    }
-    public PremiumSneaker(String description, String brand, double basePrice,
-            Carrier carrier, double conditionScore, Stack<Integer> previousOwners, double size,
-            SneakerType type, String color, LocalDate releaseDate, int userId) {
-         super(description,brand,basePrice,
-         carrier,conditionScore,previousOwners, size,
-          type,color, releaseDate, userId);
+  }
 
+  public PremiumSneaker(String description, String brand, double basePrice,
+      Carrier carrier, double conditionScore, Stack<Integer> previousOwners, double size,
+      SneakerType type, String color, LocalDate releaseDate, int userId) {
+    super(description, brand, basePrice,
+        carrier, conditionScore, previousOwners, size,
+        type, color, releaseDate, userId);
 
-    }
+  }
 
-    public PremiumSneaker(Sneaker oneSneaker) {
-        super(oneSneaker);
-    }
+  public PremiumSneaker(Sneaker oneSneaker) {
+    super(oneSneaker);
+  }
 
-    @Override
-    public PremiumSneaker clone(){
+  @Override
+  public PremiumSneaker clone() {
 
-      return new PremiumSneaker(this);
-    }
+    return new PremiumSneaker(this);
+  }
 
-   @Override
-   public double getPrice(){
-     LocalDate now = LocalDate.now();
+  @Override
+  public double getPrice() {
+    LocalDate now = LocalDate.now();
     int yearDiff = Period.between(this.getReleaseDate(), now).getYears();
 
-            return this.getBasePrice() * (1 + (yearDiff * 0.025));
-    }
+    return this.getBasePrice() * (1 + (yearDiff * 0.025));
+  }
 
 }
