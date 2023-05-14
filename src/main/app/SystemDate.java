239a5839e84d27607bdcb226f5dc691dbaaaa090
SenseiBott <jotapastore@gmail.com>
2023-05-14 18:38:32 +0100

    fix systemdate

--- src/main/app/SystemDate.java
+++ src/main/app/SystemDate.java
@@ -3,13 +3,14 @@ package app;
 import java.time.LocalDate;
 
 public class SystemDate {
-    public static LocalDate date;
+    private static LocalDate date;
 
     public static LocalDate getDate() {
         return date;
     }
+
     public static void setDate(LocalDate newDate) {
         date = newDate;
     }
-    
+
 }
