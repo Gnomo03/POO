a65a2dde7926b3d9f20b7057407094f88268f414
NunoRodriguess <nunogomesnr@gmail.com>
2023-05-14 21:38:22 +0100

    asd

--- src/main/app/Model.java
+++ src/main/app/Model.java
@@ -708,7 +708,7 @@ public class Model implements Serializable {
 
         }
 
-        if (daysBetween <= 5 || daysBetween >= 16) {
+        if (daysBetween >= 16) {
             throw new OrderNotReturnable();
         }
 
